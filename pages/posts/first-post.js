aa606a793d7b5aa873ee4f205fba5cdc7e412925
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-03-12 12:38:33 -0700

    Add styling using built in css of next

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,9 +2,11 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
 
+import Layout from '../../components/layout'
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Title Post</title>
       </Head>
@@ -13,6 +15,6 @@ export default function FirstPost() {
         <Link href='/'>Back to home</Link>
       </h2>
       <Image src='/images/profile.jpg' height={144} width={144} alt='Your Name' />
-    </>
+    </Layout>
   )
 }
